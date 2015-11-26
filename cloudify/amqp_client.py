3b8f36391095d6434ae13556c36b245d00f1065e
Isaac Shabtay <isaac@shabtay.com>
2015-11-26 05:34:11 -0800

    AMQP connection leak fix

--- cloudify/amqp_client.py
+++ cloudify/amqp_client.py
@@ -15,9 +15,9 @@
 
 
 import json
-
 import pika
 
+from cloudify import ctx
 from cloudify.utils import get_manager_ip
 
 
@@ -27,12 +27,33 @@ class AMQPClient(object):
     logs_queue_name = 'cloudify-logs'
 
     def __init__(self):
+        self._reconnect()
+
+    def publish_log(self, log):
+        self._publish(log, self.logs_queue_name)
+
+    def publish_event(self, event):
+        self._publish(event, self.events_queue_name)
+
+    def close(self):
+        if self.connection:
+            self.connection.close()
+
+    def _reconnect(self):
+        try:
+            self.close()
+        except:
+            ctx.logger.warn('Failed closing connection')
+
+        self.connection = None
         self.events_queue = None
         self.logs_queue = None
         credentials = pika.PlainCredentials('cloudify', 'c10udify')
+        ctx.logger.info('Establishing connection...')
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=get_manager_ip(),
                                       credentials=credentials))
+        ctx.logger.info('Connection established; establishing channels...')
         settings = {
             'auto_delete': True,
             'durable': True,
@@ -42,17 +63,14 @@ class AMQPClient(object):
         self.logs_queue.queue_declare(queue=self.logs_queue_name, **settings)
         self.events_queue = self.connection.channel()
         self.events_queue.queue_declare(queue=self.logs_queue_name, **settings)
-
-    def publish_log(self, log):
-        self._publish(log, self.logs_queue_name)
-
-    def publish_event(self, event):
-        self._publish(event, self.events_queue_name)
-
-    def close(self):
-        self.connection.close()
+        ctx.logger.info('Channels established; all done.')
 
     def _publish(self, item, queue):
+        if not self.connection.is_open():
+            ctx.logger.info('Connection closed; reconnecting...')
+            self._reconnect()
+            ctx.logger.info('Reconnected')
+
         self.events_queue.basic_publish(exchange='',
                                         routing_key=queue,
                                         body=json.dumps(item))
